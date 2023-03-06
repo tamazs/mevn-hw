d81191d067cd618ee8febba167c7f5f1693ae118
Tamás Major <majortamas313@gmail.com>
2023-03-06 23:24:18 +0100

    all routes added

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -8,4 +8,38 @@ router.get('/', async (req, res) => {
     res.json(todos)
 })
 
+//Create new todo
+router.post('/new', async (req, res) => {
+    const newTodo = new Todo(
+        {
+            todo: "Learn new stuff",
+            author: "me bruh"
+        }
+    );
+    const savedTodo = await newTodo.save()
+    res.json(savedTodo)
+})
+
+//Get by id
+router.get('/get/:id', async (req, res) => {
+    const todos = await Todo.findById({ _id : req.params.id })
+    res.json(todos)
+})
+
+//Update by id
+router.put('/update/:id', async (req, res) => {
+    const todosUpdate = await Todo.updateOne({
+        // { _id: req.params.id }, { $set: req.body }
+        todo: "Learn nothing",
+        author: "def not me"
+    })
+    res.json(todosUpdate)
+})
+
+//Delete by id
+router.delete('/delete/:id', async (req, res) => {
+    const todosDelete = await Todo.findByIdAndDelete({ _id : req.params.id })
+    res.json(todosDelete)
+})
+
 module.exports = router
