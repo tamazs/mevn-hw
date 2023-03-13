6377517b8f3feae3c4e33fa62bf4e8da9cf0f89f
Tamás Major <majortamas313@gmail.com>
2023-03-13 17:28:32 +0100

    installing vue project and quick test

---
+++ frontend/src/views/TodosView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>{{ test.name }}</p>
+    </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+
+const test = reactive({
+    name: "Mr Dude"
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
